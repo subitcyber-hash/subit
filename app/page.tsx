e789b6a5f5c1145de9dc9329efcb5832e83138fc
SUBI <subitfuego@gmail.com>
2026-05-09 17:36:37 +0600

    add instagram feed

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,7 @@ export default function Home() {
       <Hero />
       <About />
       <ContentShowcase />
-<InstagramFeed />
-<Socials />
+      <InstagramFeed />
       <Socials />
       <Contact />
       <Footer />
