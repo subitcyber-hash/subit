931c953ead8ab45fd7891fdcdb2d6080c3c83345
SUBI <subitfuego@gmail.com>
2026-05-09 22:33:58 +0600

    SEO basics

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://subit-three.vercel.app/sitemap.xml',
+  }
+}
